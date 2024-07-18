c0359f9d20839e36f2dad3167805a10b1f6b9536
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-07-18 15:09:24 -0400

    add product list component and fetch products

---
+++ app/products/page.tsx
@@ -0,0 +1,23 @@
+import ProductList from '../components/ProductList';
+
+async function getProducts() {
+  const res = await fetch('https://dummyjson.com/products');
+
+  if (!res.ok) {
+    throw new Error('Could no get products data');
+  }
+
+  const data = await res.json();
+  return data.products;
+}
+
+const ProductsPage = async () => {
+  const data = await getProducts();
+  return (
+    <div>
+      <ProductList productsData={data} />
+    </div>
+  );
+};
+
+export default ProductsPage;
